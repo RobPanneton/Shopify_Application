e2891aae8280a616a07d186d416d822ccb53dbb3
RobPanneton <robert.panneton@gmail.com>
2022-05-10 21:31:51 -0400

    add mui components

--- app/src/components/MainPage.js
+++ app/src/components/MainPage.js
@@ -1,32 +1,60 @@
 import React from "react";
 
+import { Container, Typography, Button, TextField } from "@mui/material";
+
 import styled from "styled-components";
 
 export const MainPage = () => {
   return (
     <>
       <MainWrapper>
-        <ContentWrapper>
-          <h1>Fun with AI</h1>
-          <p>Enter Prompt</p>
-          <input type='text'></input>
+        <ContentWrapper maxWidth='30%'>
+          <Typography variant='h3' fontWeight='600' marginBottom='23px'>
+            Fun with AI
+          </Typography>
+          <TextField
+            label='Enter Prompt'
+            multiline='true'
+            rows='3'
+            fullWidth
+            focused
+          ></TextField>
           <div className='submit-div'>
-            <button>Submit</button>
+            <Button color='primary' variant='contained'>
+              Submit
+            </Button>
           </div>
-          <h2>Responses</h2>
+          <Typography variant='h4' fontWeight='600'>
+            Responses
+          </Typography>
         </ContentWrapper>
       </MainWrapper>
     </>
   );
 };
 
-const MainWrapper = styled.div`
-  width: 100%;
+const MainWrapper = styled(Container)`
+  min-width: 100%;
+  max-width: 100%;
   min-height: 100vh;
+
+  display: flex;
+  justify-content: center;
+
+  padding: 34px 0;
+
+  font-family: "Quicksand";
 `;
 
-const ContentWrapper = styled.div`
-  width: 80%;
+const ContentWrapper = styled(Container)`
   margin: 0 auto;
   border: 1px solid black;
+  max-width: 30%;
+
+  div.submit-div {
+    display: flex;
+    justify-content: flex-end;
+
+    margin-top: 11px;
+  }
 `;

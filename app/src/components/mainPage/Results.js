fec9f10d79ea3cfc977c181ecc624988dbca663a
RobPanneton <robert.panneton@gmail.com>
2022-05-12 20:18:34 -0400

    add response cleanup script and some style for response cards

---
+++ app/src/components/mainPage/Results.js
@@ -0,0 +1,45 @@
+import React, { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
+
+import { Grid, Card, CardContent, Typography } from "@mui/material";
+
+export const Results = () => {
+  // GET CONTEXT FOR CONTENT
+  const { aiResponses } = useContext(AppContext);
+
+  console.log(aiResponses);
+
+  // RENDER THE CONTENT
+  return (
+    <>
+      <Typography variant='h4' fontWeight='600' marginBottom='17px'>
+        Responses
+      </Typography>
+      {aiResponses.length > 0 && (
+        <Grid>
+          {aiResponses.map((aiItem, index) => {
+            return (
+              <Card
+                key={index}
+                variant='outlined'
+                sx={{ width: "80%", mb: "13px" }}
+              >
+                <CardContent>
+                  <Typography variant='subtitle2' sx={{ mb: "23px" }}>
+                    {aiItem.time}
+                  </Typography>
+                  <Typography variant='h6'>
+                    Question: {aiItem.userInput}
+                  </Typography>
+                  <Typography variant='h5' style={{ whiteSpace: "pre-line" }}>
+                    {aiItem.aiResponse}
+                  </Typography>
+                </CardContent>{" "}
+              </Card>
+            );
+          })}
+        </Grid>
+      )}
+    </>
+  );
+};
---
+++ app/src/pages/MainPage.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import { Form } from "../components/mainPage/Form";
+import { Results } from "../components/mainPage/Results";
+
+import { Container } from "@mui/material";
+
+import styled from "styled-components";
+
+export const MainPage = () => {
+  return (
+    <>
+      <MainWrapper>
+        <ContentWrapper maxWidth='50%' sx={{ borderRadius: "11px" }}>
+          <Form />
+          <Results />
+        </ContentWrapper>
+      </MainWrapper>
+    </>
+  );
+};
+
+const MainWrapper = styled(Container)`
+  min-width: 100%;
+  max-width: 100%;
+  min-height: 100vh;
+
+  display: flex;
+  justify-content: center;
+
+  padding: 34px 0;
+
+  font-family: "Quicksand";
+`;
+
+const ContentWrapper = styled(Container)`
+  margin: 72px auto 0 auto;
+  max-width: 50%;
+
+  div.submit-div {
+    display: flex;
+    justify-content: flex-end;
+
+    margin: 17px 0;
+  }
+`;

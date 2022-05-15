808204cad1bc433546dfa3c33854d499d25e847f
RobPanneton <robert.panneton@gmail.com>
2022-05-15 16:54:26 -0400

    change font again

--- app/src/MuiThemeProvider.js
+++ app/src/MuiThemeProvider.js
@@ -2,6 +2,6 @@ import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
   typography: {
-    fontFamily: "Roboto, sans-serif",
+    fontFamily: "Montserrat, Roboto, sans-serif",
   },
 });
